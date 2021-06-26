9aafac39ae3d076f3b401a8753368c274b1bd0e4
Ayushi <ayushigupta8@gmail.com>
2021-06-26 17:05:31 +0530

    feat: added header ui

--- src/App.tsx
+++ src/App.tsx
@@ -5,14 +5,12 @@ function App(): React.ReactElement {
   return (
     <div className='App'>
       <Header />
-      <h1>FB Clone </h1>
     </div>
   );
 }
 
 export default App;
 
-// Header component
 // App body
 //   - side bar
 //   - feed
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,16 +1,62 @@
 import React from 'react';
+import { Avatar, IconButton } from '@material-ui/core';
+
+import SearchIcon from '@material-ui/icons/Search';
+import HomeRoundedIcon from '@material-ui/icons/HomeRounded';
+import FlagRoundedIcon from '@material-ui/icons/FlagRounded';
+import SubscriptionsRoundedIcon from '@material-ui/icons/SubscriptionsRounded';
+import StorefrontRoundedIcon from '@material-ui/icons/StorefrontRounded';
+import SupervisedUserCircleRoundedIcon from '@material-ui/icons/SupervisedUserCircleRounded';
+import AddRoundedIcon from '@material-ui/icons/AddRounded';
+import NotificationsRoundedIcon from '@material-ui/icons/NotificationsRounded';
+import ArrowDropDownRoundedIcon from '@material-ui/icons/ArrowDropDownRounded';
 
 import './Header.scss';
 
 export default function Header(): React.ReactElement {
   return (
-    <div className='Header'>
-      <div className='header-left'>
+    <div className='header'>
+      <div className='headerLeft'>
         <img src='https://1000logos.net/wp-content/uploads/2021/04/Facebook-logo.png' alt='' className='logo' />
-        <div className='search-bar'>search</div>
+        <div className='searchInput'>
+          <SearchIcon className='searchIcon' />
+          <input type='text' placeholder='Search' className='inputBar' />
+        </div>
       </div>
-      <div className='header-center'>center</div>
-      <div className='header-right'>right</div>
+
+      <div className='headerCenter'>
+        <div className='activeOption'>
+          <HomeRoundedIcon fontSize='large' className='icon' />
+        </div>
+        {/* <div className='option'>
+          <FlagRoundedIcon fontSize='large' />
+        </div> */}
+        <div className='option'>
+          <SubscriptionsRoundedIcon fontSize='large' className='icon' />
+        </div>
+        <div className='option'>
+          <StorefrontRoundedIcon fontSize='large' className='icon' />
+        </div>
+        <div className='option'>
+          <SupervisedUserCircleRoundedIcon fontSize='large' className='icon' />
+        </div>
+      </div>
+
+      <div className='headerRight'>
+        <div className='info'>
+          <Avatar />
+          <h4>User name</h4>
+        </div>
+        <IconButton>
+          <AddRoundedIcon />
+        </IconButton>
+        <IconButton>
+          <NotificationsRoundedIcon />
+        </IconButton>
+        <IconButton>
+          <ArrowDropDownRoundedIcon />
+        </IconButton>
+      </div>
     </div>
   );
 }
