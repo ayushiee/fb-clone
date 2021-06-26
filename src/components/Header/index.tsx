78100bbed43d217a59fc532a5c88395db8ebb9f4
Ayushi <ayushigupta8@gmail.com>
2021-06-26 17:06:44 +0530

    chore: icon consolidation

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,15 +1,17 @@
 import React from 'react';
 import { Avatar, IconButton } from '@material-ui/core';
 
-import SearchIcon from '@material-ui/icons/Search';
-import HomeRoundedIcon from '@material-ui/icons/HomeRounded';
-import FlagRoundedIcon from '@material-ui/icons/FlagRounded';
-import SubscriptionsRoundedIcon from '@material-ui/icons/SubscriptionsRounded';
-import StorefrontRoundedIcon from '@material-ui/icons/StorefrontRounded';
-import SupervisedUserCircleRoundedIcon from '@material-ui/icons/SupervisedUserCircleRounded';
-import AddRoundedIcon from '@material-ui/icons/AddRounded';
-import NotificationsRoundedIcon from '@material-ui/icons/NotificationsRounded';
-import ArrowDropDownRoundedIcon from '@material-ui/icons/ArrowDropDownRounded';
+import {
+  SearchIcon,
+  HomeRoundedIcon,
+  FlagRoundedIcon,
+  SubscriptionsRoundedIcon,
+  SupervisedUserCircleRoundedIcon,
+  AddRoundedIcon,
+  ArrowDropDownRoundedIcon,
+  NotificationsRoundedIcon,
+  StorefrontRoundedIcon
+} from '../../utils/icons';
 
 import './Header.scss';
 
---
+++ src/utils/icons.ts
@@ -0,0 +1,21 @@
+import SearchIcon from '@material-ui/icons/Search';
+import HomeRoundedIcon from '@material-ui/icons/HomeRounded';
+import FlagRoundedIcon from '@material-ui/icons/FlagRounded';
+import SubscriptionsRoundedIcon from '@material-ui/icons/SubscriptionsRounded';
+import StorefrontRoundedIcon from '@material-ui/icons/StorefrontRounded';
+import SupervisedUserCircleRoundedIcon from '@material-ui/icons/SupervisedUserCircleRounded';
+import AddRoundedIcon from '@material-ui/icons/AddRounded';
+import NotificationsRoundedIcon from '@material-ui/icons/NotificationsRounded';
+import ArrowDropDownRoundedIcon from '@material-ui/icons/ArrowDropDownRounded';
+
+export {
+  SearchIcon,
+  HomeRoundedIcon,
+  FlagRoundedIcon,
+  SubscriptionsRoundedIcon,
+  SupervisedUserCircleRoundedIcon,
+  AddRoundedIcon,
+  ArrowDropDownRoundedIcon,
+  NotificationsRoundedIcon,
+  StorefrontRoundedIcon
+};
