fc51d80d827f01a00abcbf305923bab6a175619b
Ayushi <ayushigupta8@gmail.com>
2021-07-02 15:22:50 +0530

    chore: widget ui fix

--- src/components/Widget/index.tsx
+++ src/components/Widget/index.tsx
@@ -6,9 +6,9 @@ function Widget(): React.ReactElement {
   return (
     <div className='widget'>
       <iframe
-        className='widget__scroll'
+        className='widgetIframe'
         title='facebook-post'
-        src='https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2FakkyProjects%2F&tabs=timeline&width=340&height=1500&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=true&appId'
+        src='https://www.facebook.com/plugins/page.php?href=https://www.facebook.com/nytimes/&tabs=timeline&width=340&height=1500&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=true&appId'
         width='340'
         height='100%'
         style={{ border: 'none', overflow: 'hidden' }}
